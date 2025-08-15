135ab41e69ea629d58029da2591820aa83e37184
Sadid Hasan Rakib <sadid.hasan14@gmail.com>
2025-08-15 19:35:55 +0600

    implement user login & logout functionality

--- app/Http/Controllers/API/V1/AuthController.php
+++ app/Http/Controllers/API/V1/AuthController.php
@@ -3,14 +3,38 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Resources\V1\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(UserLoginRequest $request)
     {
+        $validated = $request->validated();
+
+        if (!Auth::attempt($validated)) {
+            return response()->json([
+                'message' => 'Invalid credentials!',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = User::where('username', $validated['username'])->first();
+
+        return response()->json([
+            'message' => 'Welcome back!',
+            'user'    => new UserResource($user),
+        ]);
     }
 
     public function logout()
     {
+        Auth::user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'See ya next time!',
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,13 @@
 
 use App\Http\Controllers\API\V1\AuthController;
 use App\Http\Controllers\API\V1\RegisterController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
     Route::post('register', RegisterController::class);
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
 });
